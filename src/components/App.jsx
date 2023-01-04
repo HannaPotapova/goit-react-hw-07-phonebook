682a209ed6c29b4858927826f9643ffc5e1d1312
Hanna Potapova <103482918+HannaPotapova@users.noreply.github.com>
2023-01-04 12:35:51 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import css from "components/App.module.css";
+import { useSelector } from "react-redux";
+import { stateContacts } from "redux/contactSlice";
+
+const App = () => {
+  const contacts = useSelector(stateContacts);
+  return (
+      <div>
+        <div className={css.box}>
+          <h1>Phonebook</h1>
+          <ContactForm />
+        </div>
+      {contacts.length ? (
+        <div className={css.box}>
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList />
+        </div>
+      ) : (<h2>No contacts</h2>)}
+      </div>
+    )  
+}
+
+export default App;
